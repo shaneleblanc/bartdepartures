e6f065497016fc58a585fe068ba22ab456990843
Shane LeBlanc <shane@xs.vc>
2019-01-30 11:26:58 -0800

    list_stations() and readme

--- bdep.py
+++ bdep.py
@@ -1,20 +1,59 @@
 import requests
 import json
+import sys
 
-station = 'POWL'
-r = requests.get('http://api.bart.gov/api/etd.aspx?cmd=etd&orig='+station+'&key=MW9S-E7SL-26DU-VV8V&json=y')
-j = json.loads(r.text)
 
-print('Trains from '+station+' station:')
-for item in j['root']['station'][0]['etd']:
-    destination = item['destination']
-    trains = []
-    est = item['estimate']
-    for e in est:
-        if e['minutes'] == 'Leaving':
-            trains.append('Leaving now')
+def get_stations():
+    r = requests.get('http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y')
+    j = json.loads(r.text)
+    stations = {s['name']: s['abbr'] for s in j['root']['stations']['station']}
+    return stations
+
+
+def get_departures(abbr, name):
+    try:
+        r = requests.get('http://api.bart.gov/api/etd.aspx?cmd=etd&orig='+abbr+'&key=MW9S-E7SL-26DU-VV8V&json=y')
+        j = json.loads(r.text)
+
+        print('Trains from '+name+' station:\n')
+        for item in j['root']['station'][0]['etd']:
+            destination = item['destination']
+            trains = []
+            est = item['estimate']
+            for e in est:
+                if e['minutes'] == 'Leaving':
+                    trains.append('Leaving now')
+                else:
+                    trains.append(e['minutes'] + ' minutes')
+
+            print(destination + ': ' + ', '.join(trains))
+    except:
+        print("Invalid station or unknown input.")
+
+
+def list_stations():
+    stations = get_stations()
+    for name, abbr in stations.items():
+        print(abbr + ' | ' + name)
+
+
+def main():
+    stations = get_stations()
+    stations_list = []
+    for i, v in enumerate(stations.keys()):
+        print(i, v)
+        stations_list.append(v)
+
+    station_number = int(input("Please choose a station: "))
+    print('\n')
+    get_departures(stations[stations_list[station_number]], stations_list[station_number])
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        main()
+    elif len(sys.argv) == 2:
+        if sys.argv[1] == 'list':
+            list_stations()
         else:
-            trains.append(e['minutes'] + ' minutes')
-
-    print(destination + ' : ' + ', '.join(trains))
-
+            get_departures(sys.argv[1], sys.argv[1])
---
+++ readme.md
@@ -0,0 +1,7 @@
+# BART Departure Times CLI Tool
+### Description
+Uses the public BART API to pull station information and check for trains leaving from the station chosen. Compatible with Python 3.*, Python 2 works but will output less presentable strings.
+### Instructions 
+- Execute with `python3 bdep.py` to see a list of stations. Enter the number of the station from the list of stations that you would like to see departure times for. 
+- Execute with `python3 bdep.py [STATION_ABBREVIATION]` to skip listing stations and output departure times for a specific station. 
+- Execute `python3 bdep.py list` to see a list of stations and their abbreviations
